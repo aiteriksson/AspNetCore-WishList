77e0acd30b89ad04659128bd2139a57c221a2a57
aiteriksson <56494453+aiteriksson@users.noreply.github.com>
2020-09-02 12:16:52 -0500

    namespace

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace WishList
+namespace WishList.Controllers
 {
-    public class HomeController: Controller 
+    public class HomeController : Controller 
     {
         public IActionResult Index()
         {
